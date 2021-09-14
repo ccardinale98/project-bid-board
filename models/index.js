89a2f9db80c7d55f6da58534982c7a6caff7f60c
ccardinale98 <ccardinale98@gmail.com>
2021-09-14 17:15:12 -0400

    merged bidder and poster into user

--- models/index.js
+++ models/index.js
@@ -1,56 +1,31 @@
-const Poster = require('./Poster');
-const Bidder = require('./Bidder');
+const User = require('./User');
 const Project = require('./Project');
 const Bid = require('./Bid');
 
-//Poster -> Project
-// ---------------------------------------------------------
-Poster.hasMany(Project, {
+User.belongsToMany(Project, {
+    through: {
+        model: Bid,
+        unique: false
+    },
+
+    as: 'project_users'
+});
+
+Project.belongsToMany(User, {
+    through: {
+        model: Bid,
+        unique: false
+    },
+
+    as: 'bids'
+});
+
+User.hasMany(Project, {
     foreignKey: 'poster_id'
 })
 
-Project.belongsTo(Poster, {
+Project.belongsTo(User, {
     foreignKey: 'poster_id'
 })
 
-//Bidder -> Project
-// ---------------------------------------------------------
-Bidder.hasMany(Project, {
-    foreignKey: 'bidder_id',
-})
-
-Project.belongsTo(Bidder, {
-    foreignKey: 'bidder_id'
-})
-
-//Project -> Bid
-// ---------------------------------------------------------
-Project.hasMany(Bid, {
-    foreignKey: 'project_id'
-})
-
-Bid.belongsTo(Project, {
-    foreignKey: 'project_id'
-})
-
-//Bidder -> Bid
-// ---------------------------------------------------------
-Bidder.hasMany(Bid, {
-    foreignKey: 'bidder_id',
-})
-
-Bid.belongsTo(Bidder, {
-    foreignKey: 'bidder_id'
-})
-
-//Poster -> Bidder
-// ---------------------------------------------------------
-// Poster.hasMany(Bidder, {
-//     foreignKey: 'poster_id'
-// })
-
-// Bidder.belongsTo(Poster, {
-//     foreignKey: 'poster_id'
-// })
-
-module.exports = { Poster, Bidder, Project, Bid }
+module.exports = { User, Project, Bid }
--- seeds/seed.js
+++ seeds/seed.js
@@ -1,20 +1,14 @@
 const sequelize = require('../config/connection');
-const { Poster, Bidder, Project, Bid } = require('../models');
+const { User, Project, Bid } = require('../models');
 
-const posterData = require('./poster.json');
-const bidderData = require('./bidder.json');
+const userData = require('./user.json');
 const projectData = require('./project.json');
 const bidData = require('./bid.json');
 
 const seedDatabase = async () => {
-    await sequelize.sync({ force: true });
+    await sequelize.sync({ force: false });
 
-    await Poster.bulkCreate(posterData, {
-      individualHooks: true,
-      returning: true
-    });
-    
-    await Bidder.bulkCreate(bidderData, {
+    await User.bulkCreate(userData, {
       individualHooks: true,
       returning: true
     });
